356a5bdd72b13e05146275f3f5ffdae181c40e82
Max Sveshnikov <msveshnikov@gmail.com>
2024-09-07 23:09:50 +0200

    WIP

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {  } from 'react';
 import {
     Container,
     Typography,
@@ -8,31 +8,19 @@ import {
     ListItemAvatar,
     Avatar,
     Paper,
-    CircularProgress
+    Box
 } from '@mui/material';
 import apiService from '../services/apiService';
+import { Helmet } from 'react-helmet';
+import { useQuery } from 'react-query';
+import Loading from '../components/Loading';
 
 const Leaderboard = () => {
-    const [leaderboardData, setLeaderboardData] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const getLeaderboardData = async () => {
-            try {
-                const data = await apiService.fetchLeaderboard();
-                setLeaderboardData(data);
-                setLoading(false);
-            } catch (error) {
-                console.error(
-                    'Ошибка при получении данных таблицы лидеров:',
-                    error
-                );
-                setLoading(false);
-            }
-        };
-
-        getLeaderboardData();
-    }, []);
+    const {
+        data: leaderboardData,
+        isLoading,
+        error
+    } = useQuery('leaderboard', apiService.fetchLeaderboard);
 
     const translations = {
         title: 'Таблица лидеров',
@@ -41,45 +29,58 @@ const Leaderboard = () => {
         score: 'Очки'
     };
 
-    if (loading) {
+    if (isLoading) return <Loading />;
+    if (error)
         return (
-            <Container
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: '100vh'
-                }}
-            >
-                <CircularProgress />
-            </Container>
+            <Typography color="error">
+                Ошибка при загрузке таблицы лидеров: {error.message}
+            </Typography>
         );
-    }
 
     return (
-        <Container maxWidth="sm" sx={{ mt: 4 }}>
-            <Typography variant="h4" component="h1" gutterBottom align="center">
-                {translations.title}
-            </Typography>
-            <Paper elevation={3}>
-                <List>
-                    {leaderboardData.map((user, index) => (
-                        <ListItem
-                            key={user.id}
-                            divider={index !== leaderboardData.length - 1}
-                        >
-                            <ListItemAvatar>
-                                <Avatar>{index + 1}</Avatar>
-                            </ListItemAvatar>
-                            <ListItemText primary={user.name} />
-                            <Typography variant="body2">
-                                {user.score}
-                            </Typography>
-                        </ListItem>
-                    ))}
-                </List>
-            </Paper>
-        </Container>
+        <>
+            <Helmet>
+                <title>Таблица лидеров - Изучение чешского языка</title>
+                <meta
+                    name="description"
+                    content="Просмотрите таблицу лидеров и соревнуйтесь с другими учениками чешского языка."
+                />
+            </Helmet>
+            <Container maxWidth="sm">
+                <Box my={4}>
+                    <Typography
+                        variant="h4"
+                        component="h1"
+                        gutterBottom
+                        align="center"
+                    >
+                        {translations.title}
+                    </Typography>
+                    <Paper elevation={3}>
+                        <List>
+                            {leaderboardData.map((user, index) => (
+                                <ListItem
+                                    key={user._id}
+                                    divider={
+                                        index !== leaderboardData.length - 1
+                                    }
+                                >
+                                    <ListItemAvatar>
+                                        <Avatar>{index + 1}</Avatar>
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary={user.username || user.email}
+                                    />
+                                    <Typography variant="body2">
+                                        {user.leaderboardScore}
+                                    </Typography>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Paper>
+                </Box>
+            </Container>
+        </>
     );
 };
 
